6509590e2a2ea1764b08a036ea152a4e6c14f63a
Marco Peters <marco.peters@brockmann-consult.de>
2022-10-18 11:23:42 +0200

    ongoing implementation
    introduced ortho metadata

--- src/main/java/org/esa/snap/opt/dataio/enmap/EnmapL1CMetadata.java
+++ src/main/java/org/esa/snap/opt/dataio/enmap/EnmapL1CMetadata.java
@@ -1,30 +1,14 @@
 package org.esa.snap.opt.dataio.enmap;
 
 import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
 
 import javax.xml.xpath.XPath;
-import java.awt.*;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
-class EnmapL1CMetadata extends EnmapMetadata {
+class EnmapL1CMetadata extends EnmapOrthoMetadata {
 
     EnmapL1CMetadata(Document doc, XPath xPath) {
         super(doc, xPath);
-    }
-
-    @Override
-    public Dimension getSceneDimension() throws IOException {
-        int width = Integer.parseInt(getNodeContent("/level_X/specific/widthOfScene"));
-        int height = Integer.parseInt(getNodeContent("/level_X/specific/heightOfScene"));
-        return new Dimension(width, height);
-    }
-
-    @Override
-    public double getPixelSize() throws IOException {
-        return Integer.parseInt(getNodeContent("/level_X/specific/pixelSizeOfOrthoScene"));
     }
 
     @Override
@@ -37,27 +21,4 @@ class EnmapL1CMetadata extends EnmapMetadata {
         return "W/m^2/sr/nm";
     }
 
-    @Override
-    public Map<String, String> getFileNameMap() throws IOException {
-        NodeList nodeSet = getNodeSet("/level_X/product/productFileInformation/*/name");
-
-        HashMap<String, String> map = new HashMap<>();
-        map.put(EnmapFileUtils.METADATA_KEY, getFileName(EnmapFileUtils.METADATA_KEY, nodeSet));
-        map.put(EnmapFileUtils.SPECTRAL_IMAGE_KEY, getFileName(EnmapFileUtils.SPECTRAL_IMAGE_KEY, nodeSet));
-        map.put(EnmapFileUtils.QUALITY_CLASSES_KEY, getFileName(EnmapFileUtils.QUALITY_CLASSES_KEY, nodeSet));
-        map.put(EnmapFileUtils.QUALITY_CLOUD_KEY, getFileName(EnmapFileUtils.QUALITY_CLOUD_KEY, nodeSet));
-        map.put(EnmapFileUtils.QUALITY_CLOUDSHADOW_KEY, getFileName(EnmapFileUtils.QUALITY_CLOUDSHADOW_KEY, nodeSet));
-        map.put(EnmapFileUtils.QUALITY_HAZE_KEY, getFileName(EnmapFileUtils.QUALITY_HAZE_KEY, nodeSet));
-        map.put(EnmapFileUtils.QUALITY_CIRRUS_KEY, getFileName(EnmapFileUtils.QUALITY_CIRRUS_KEY, nodeSet));
-        map.put(EnmapFileUtils.QUALITY_SNOW_KEY, getFileName(EnmapFileUtils.QUALITY_SNOW_KEY, nodeSet));
-        map.put(EnmapFileUtils.QUALITY_TESTFLAGS_KEY, getFileName(EnmapFileUtils.QUALITY_TESTFLAGS_KEY, nodeSet));
-        map.put(EnmapFileUtils.QUALITY_PIXELMASK_KEY, getFileName(EnmapFileUtils.QUALITY_PIXELMASK_KEY, nodeSet));
-
-        return map;
-    }
-
-    @Override
-    public int getNumSpectralBands() throws IOException {
-        return Integer.parseInt(getNodeContent("/level_X/product/image/merge/channels"));
-    }
 }
